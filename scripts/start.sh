e078279ae25bde85ce1efb7af4ce8eb18b2917ca
NuwanAWS <awscertifieddevops@gmail.com>
2025-10-04 21:13:46 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-cd /home/ec2-user/app
-java -jar $(ls *.jar | head -n 1) > app.log 2>&1 &
+set -e
+
+APP_DIR=/home/ec2-user/app
+cd $APP_DIR
+
+# Find shaded jar (fat jar from Maven Shade Plugin)
+JAR=$(ls *-shaded.jar | head -n 1)
+
+if [ -z "$JAR" ]; then
+  echo "ERROR: No shaded jar found in $APP_DIR"
+  exit 1
+fi
+
+echo "Starting $JAR ..."
+nohup java -jar "$JAR" > app.log 2>&1 &
